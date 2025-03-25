b0dbdb73daac804d3096a003f224ba2e0e1e47dc
medupipub <info@medupipub.com>
2025-03-25 08:56:09 +0200

    Big Sweep
    Theo Updates, along with development of Updates Page

--- script.js
+++ script.js
@@ -224,3 +224,10 @@ carouselItems.forEach(item => {
   
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("footer.html")
+      .then(response => response.text())
+      .then(data => document.getElementById("footer-container").innerHTML = data)
+      .catch(error => console.error("Error loading footer:", error));
+});
+
